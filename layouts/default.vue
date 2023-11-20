8ff3ffbd013aff8c2251d7d03f5876f86d446394
NR Habibi <nrhbb14@gmail.com>
2023-11-20 11:27:23 +0800

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Nav>Default layout</Nav>
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
