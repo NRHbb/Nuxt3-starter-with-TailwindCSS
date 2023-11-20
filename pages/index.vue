f6fce16101feb4e028ff7fa2244fd79ebf827822
NR Habibi <nrhbb14@gmail.com>
2023-11-20 11:24:34 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center justify-center m-auto min-h-screen w-full">
+        <h1 class="text-5xl font-extrabold text-black drop-shadow-xl text-center">Welcome To Nuxt 3 Basic Starter Pack with Tailwind Css</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
