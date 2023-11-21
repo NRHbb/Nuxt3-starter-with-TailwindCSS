5c22a79628d99b6911ff8c908030ebf105ac1ffa
NR Habibi <nrhbb14@gmail.com>
2023-11-21 09:58:40 +0800

    change href to NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="flex items-center justify-center m-auto min-h-screen w-full flex-col gap-4">
         <h1 class="text-5xl font-extrabold text-black drop-shadow-xl text-center">Welcome To Nuxt 3 Basic Starter Pack with Tailwind Css</h1>
-        <a href="https://nuxt.com/" class="text-center text-black">More documentation</a>
+        <NuxtLink to="https://nuxt.com/">
+            <h3>More Documentation</h3>
+        </NuxtLink>
+        <!-- <a href="https://nuxt.com/" class="text-center text-black">More documentation</a> -->
     </div>
 </template>
 
